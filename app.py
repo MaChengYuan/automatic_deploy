b4cd138a7d229ce1227ffb45e5515f64d0e32a10
fernando <tommy0101@hotmail.com.tw>
2024-07-24 23:22:47 +0800

    port 5000

--- app.py
+++ app.py
@@ -28,3 +28,4 @@ todos = db.todos # creating a collection called "todos"
 # The dunder if __name__ code block
 if __name__ == "__main__":
     app.run(debug=True)
+    # app.run(host = '0.0.0.0' , port = 8080)
